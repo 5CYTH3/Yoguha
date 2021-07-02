275b994e46fb23fc5fee3d31f373b0cd6f55debd
5CYTH3 <ScytheD@outlook.fr>
2021-07-02 23:55:09 +0200

    🎐 Trying to enable firebase on the project...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'src/views/home_screen.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
   @override
