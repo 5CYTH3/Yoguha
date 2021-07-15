a8a7ab60934ac64260475b76b64e81f3e023ab64
5CYTH3 <ScytheD@outlook.fr>
2021-07-15 12:31:42 +0200

    🎏Updated the app name + added a temporary screen on the tools page.

--- lib/src/views/tools_screen.dart
+++ lib/src/views/tools_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dribbly/src/components/OwnAppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ToolsScreen extends StatelessWidget {
   const ToolsScreen({ Key? key }) : super(key: key);
@@ -19,8 +20,15 @@ class ToolsScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: Theme.of(context).accentColor),
       child: Column(
-        
+        children: [
+          Text("Not available for now", style: GoogleFonts.alegreya(color: Colors.white, fontSize: 25), textAlign: TextAlign.center),
+          Text("You can't access the tool page right now. Be patient, It will come later if I can implement it.", style: GoogleFonts.alegreyaSans(color: Colors.white70, fontSize: 20), textAlign: TextAlign.center)
+        ],
       ),
     );
   }
